76cfde8ab5a0de20edc2f29b712283005c86d86e
Nicogonzap <nicolasgonzap@gmail.com>
2021-10-22 17:49:06 -0300

    agregada estrucuta de carpetas, modificado app.js para vincular con public y html, resuelta vinculación entre html y css

--- app.js
+++ app.js
@@ -3,12 +3,12 @@ const app = express()
 const path = require('path')
 const port = 3000
 
-const path_public = path.resolve(__dirname, 'public')
+const pathHTML = path.join(__dirname, '/views/')
 
-app.use(express.static(path_public))
+app.use(express.static('public'));
 
 app.get('/',(req,res) => {
-    res.sendFile(path.join(__dirname, '/views/home.html'))
+    res.sendFile(path.join(pathHTML + 'home.html'))
 })
 
 app.listen(port, () => console.log(`Servidor en el puerto: ${port}`))
