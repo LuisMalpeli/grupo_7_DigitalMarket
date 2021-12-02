a3dfbec200e5b07ce07043aec6cd3ebc2dd72463
Luis <malpeliluis@gmail.com>
2021-12-02 00:13:42 -0300

    inicio sprint

--- src/app.js
+++ src/app.js
@@ -1,4 +1,6 @@
 const express = require('express')
+const cookieParser = require('cookie-parser');
+const methodOverride =  require('method-override');
 const app = express()
 const path = require('path')
 const port = 3000
@@ -11,6 +13,14 @@ const userRouter = require('./routes/user')
 app.set('view engine', 'ejs');
 app.set('views', path.resolve(__dirname,'views'));
 
+//JSON
+app.use(express.json());
+app.use(cookieParser());
+
+//METODOS
+app.use(methodOverride('_method'));
+
+
 app.listen(process.env.PORT || port, ()=>{
     console.log('Servidor corriendo en el puerto : ',port);
 });
@@ -19,21 +29,3 @@ app.use(express.static(path.resolve(__dirname,'../public')));
 app.use('/',mainRouter)
 app.use('/product', productRouter)
 app.use('/user',userRouter)
-
-/*
-app.get('/login',(req,res) => {
-    res.sendFile(path.join(pathHTML + 'login.html'))
-})
-
-app.get('/register',(req,res) => {
-    res.sendFile(path.join(pathHTML + 'register.html'))
-})
-
-app.get('/productDetail',(req,res) => {
-    res.sendFile(path.join(pathHTML + 'productDetail.html'))
-})
-
-app.get('/productCart',(req,res) => {
-    res.sendFile(path.join(pathHTML + 'productCart.html'))
-})
-*/
