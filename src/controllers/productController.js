b0ad37607ac33bd3ef698025d1984424ca10e90b
Nicogonzap <nicolasgonzap@gmail.com>
2021-12-05 22:43:44 -0300

    Agregado listado de productos y modificado form de register

--- src/controllers/productController.js
+++ src/controllers/productController.js
@@ -1,5 +1,16 @@
+const fs = require('fs');
+const path = require('path');
+
+const productsFilePath = path.join(__dirname, '../db/products.json');
+const productos = JSON.parse(fs.readFileSync(productsFilePath, 'utf-8'));
 
 const productController = {
+    main: function(req, res) {
+        res.render(
+            'products/products',
+            {productos: productos}
+            )
+    },
     productDetail: function(req, res) {
         res.render('products/productDetail') //Dentro de la carpeta products, busca product Detail
     },
--- src/routes/product.js
+++ src/routes/product.js
@@ -2,6 +2,8 @@ const express = require('express')
 const router = express.Router();
 const productController = require('../controllers/productController')
 
+//Capturo lo que venga a products y le muestro la lista de productos completa
+router.get('/', productController.main); //--> Todavía no está seteada esta vista
 router.get('/productDetail', productController.productDetail);
 router.get('/productCart',productController.productCart);
 router.get('/productCreate',productController.productCreate)
