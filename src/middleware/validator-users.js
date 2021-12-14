bb1640bec18a3c225b7f7df5e7f84c5999c17616
Luis <malpeliluis@gmail.com>
2021-12-14 18:30:42 -0300

    validaciones y otras cosas

---
+++ src/middleware/validator-users.js
@@ -0,0 +1,19 @@
+const {check} = require('express-validator')
+
+module.exports = [
+    //Validar Users
+    check('user')
+        .notEmpty().withMessage('Debe completar el campo usuario').bail()
+        .isLength({min:5, max:20}).withMessage('El usuario debe tener un minimo de 5 caracteres y maximo de 20'),
+    check('fullName')
+        .notEmpty().withMessage('Debe completar el campo nombre y apellido').bail()
+        .isLength({min:8, max:30}).withMessage('El usuario debe tener un minimo de 8 caracteres y maximo de 30'),
+    check('gender')
+        .notEmpty().withMessage('Debe elegir un genero'),
+    check('email')
+        .notEmpty().withMessage('Debe completar el campo email').bail()
+        .isEmail().withMessage('Ingrese un email valido'),
+    check('pass')
+        .notEmpty().withMessage('Debe completar el campo contraseña').bail()
+        .isLength({min:6, max:10}).withMessage('La contraseña debe tener un minimo de 6 caracteres y maximo de 10'),
+    ]
--- src/routes/usersRouter.js
+++ src/routes/usersRouter.js
@@ -2,10 +2,11 @@ const express = require('express')
 const router = express.Router();
 const userController = require('../controllers/userController')
 const upload = require('../middleware/multer');
+const validator = require('../middleware/validator-users')
 
 //Registro de usuario
 router.get('/register', userController.register)
-router.post('/register', upload.single('avatar'), userController.registerSend)
+router.post('/register', upload.single('avatar'), validator, userController.registerSend)
 
 //Login de usuario
 router.get('/login', userController.login)
