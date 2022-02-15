9ff351667f4ad9d9b5b37dd5dec3a9282321f03d
Nicogonzap <nicolasgonzap@gmail.com>
2022-02-14 21:01:07 -0300

    arreglados bugs de login y register

--- src/middleware/recordarme.js
+++ src/middleware/recordarme.js
@@ -5,7 +5,7 @@ function recordarmeMiddleware(req, res, next) {
     //Si el usuario NO está logueado, pero hizo click en recordarme
     if(req.session.usuarioLogueado == undefined && req.cookies.recordarme != undefined) {
         //La cookie solo almacena el mail del usuario, por lo que debo buscarlo para loguearlo
-        db.Usuario.findOne({
+        db.Usuarios.findOne({
             where: {
                 email: req.cookies.recordarme
             }
