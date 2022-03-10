437ec610501347c89a032be0b218ea98b372c1b4
Luis <malpeliluis@gmail.com>
2022-03-10 14:25:17 -0300

    api

---
+++ src/controllers/api_controller.js
@@ -0,0 +1,44 @@
+const db = require('../database/models')
+const Sequelize = require("sequelize");
+const Op = Sequelize.Op;
+
+module.exports = {
+    userList: (req,res) => {
+        db.Usuarios.findAll()
+        .then(users => {
+            return res.json({
+                count: users.lenght,
+                // countByCategory ---> no entendi que quiere que haga
+                users: Array.from(users)
+            })
+        })
+    },
+    userId: (req,res) => {
+        db.Usuarios.findByPk(req.params.id)
+        .then(user => {
+            return res.json(user)
+        })
+    },
+    userDelete:(req,res) => {
+
+    },
+    productList: (req,res) => {
+        db.Productos.findAll()
+        .then(products => {
+            return res.json({
+                count: products.lenght,
+                // countByCategory ---> no entendi que quiere que haga
+                products: Array.from(products)
+            })
+        })
+    },
+    productId: (req,res) => {
+        db.Productos.findByPk(req.params.id)
+        .then(product => {
+            return res.json(product)
+        })
+    },
+    productDelete:(req,res) => {
+
+    },
+}
