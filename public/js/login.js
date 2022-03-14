afb71255f859f1c824b9a4cd74c1a06a8a80b00d
Nicogonzap <nicolasgonzap@gmail.com>
2022-03-13 23:04:27 -0300

    validaciones front para login

--- public/js/login.js
+++ public/js/login.js
@@ -1,43 +1,76 @@
 window.onload = function() {
 
-    const form = document.querySelector('#formulario')
-    const ul = document.querySelector('#area-errores')
+    const form = document.querySelector('#formulario');
 
-    /* function validar(elemento) {   
+    const errores = [];
+    const camposConError = []
 
-        let li = ul.querySelector(`#${elemento.getAttribute('name')}`)
+    function validar(elemento) {   
+
        
         switch (elemento.getAttribute('name')) {
             case 'email':
                 if (elemento.value == '') {
-                    li.innerText = 'El campo Email no puede estar vacio'
+                    errores.push({email:'El campo Email no puede estar vacio'})
                 } else if (!elemento.value.includes('@')) { // false si no lo encontro
-                    li.innerText = 'El campo Email no es valido (falta @)'
-                } else {
-                    li.innerText = ''
+                    errores.push({email: 'El campo Email no es valido (falta @)'})
                 }
             break
             case 'password':
                 if (elemento.value == '') {
-                    li.innerText = 'El campo Contrasena no puede estar vacio'
-                } else {
-                    li.innerText = ''
+                    errores.push({password: 'El campo Contraseña no puede estar vacio'})
                 }
             break
         }  
 
     }
 
-    form.addEventListener('change', () => {
-        form.querySelectorAll('input').forEach(elemento => {
-            validar(elemento)
-        })
-    })
 
     form.addEventListener('submit', (e) => {
         e.preventDefault()
         form.querySelectorAll('input').forEach(elemento => {
             validar(elemento)
         })
-    }) */
+        if (errores.length > 1) {
+            camposConError.length = 0; //inicia nuevamente los campos con error (camposConError almacena los nombres de los input que tienen error)
+            //el forEach completa el array de campos con error según las keys de cada objeto en el array de errores
+            errores.forEach(error => camposConError.push(Object.keys(error)[0])); //Cada posición del array contendrá solo 1 error
+            //Object.keys(objeto) creará un array con las keys del objeto
+            //esto arrojará un output del estilo [fullName, email, avatar, password]
+            
+            let inputFields = document.querySelectorAll('.user-input'); //toma el div donde se encuentran los inputs
+            let campoConError = null
+            
+            inputFields.forEach(field =>{
+                //Itera sobre los div con clase user-input para ver si en alguno de los inputs dentro hay errores
+                nombreCampo = field.querySelector('input').name //Toma el nombre del input dentro del div evaluado
+                
+                //Busca si el input actual es parte del array con errores
+                campoConError = camposConError.find(campo => campo == nombreCampo) 
+                
+                let parrafo = field.querySelector('p');
+                if (campoConError) {
+                    //si campoConError retorna con un valor distinto de undefined, quiere decir que el campo tiene errores
+                    //msjError almacenará el mensaje del error que coincida con el campo que estamos mirando ahora
+                    let msjError = errores.find(objetoError => Object.keys(objetoError)[0] == campoConError)[campoConError];
+                    //Agrega un párrafo con error al final del div indicando el mensaje de error correspondiente
+                    
+                    if (parrafo !== null) {
+                        parrafo.innerText = msjError
+                    } else {
+                        field.innerHTML += `<p class="errores">${msjError}</p>`
+                    }
+                } else {
+                    if (parrafo !== null) {
+                        parrafo.parentNode.removeChild(parrafo)
+                    }
+                }
+            })
+
+
+        } else {
+            form.submit();
+        }
+
+    })
 }
